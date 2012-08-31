19f65a890addb0d2fa22930d2e72218d4ff2c746
Javier Pino <savagej.p@gmail.com>
2012-08-31 10:00:18 -0430

    No funciona el ajax en bb 6.0 probando OTRA SOLUCION

--- assets/js/jquery.mobile.td.custom.js
+++ assets/js/jquery.mobile.td.custom.js
@@ -95,7 +95,7 @@ function iniciar_submit() {
     if (!password) {
         setErrorMessage("El campo 'Contraseña' es Obligatorio");        
     }
-         
+            
     $.ajax({
         url: server + '/td/restful/account/login',
         type: 'POST',
@@ -112,7 +112,7 @@ function iniciar_submit() {
                     'Bienvenid@, ' + json.user.realname +
                     ', ingresaste exitosamente usando tu correo: ' + json.user.email);                
                 
-                TBL_User.all().one(null, function (one) {                    
+                /*TBL_User.all().one(null, function (one) {                    
                     
                     if (one != null) {                        
                         persistence.remove(one);  
@@ -129,7 +129,7 @@ function iniciar_submit() {
                         $.mobile.hidePageLoadingMsg ();                                                              
                     });
                     
-                });                
+                });   */             
              } else {
                  setErrorMessage(json.message);
                  $.mobile.hidePageLoadingMsg ();          
@@ -141,7 +141,26 @@ function iniciar_submit() {
             setErrorMessage('No es posible conectarse al servidor');            
             $.mobile.hidePageLoadingMsg ();                      
         }   
-     });     
+    }); 
+     
+    /*http.open("GET", url, true);
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            //alert(http.responseText);
+            var json = JSON.parse(http.responseText);
+            $.each(json, function(i, object) {
+                var cr = "<li id='menuList'><a id="+object.id+"  data-transition='slide' class='menuClass' ><img src=css/images/"+object.id+".png /> <h3> "+object.menuname+" </h3></a></li>";
+                $("#mainMenu").append(cr);
+                $("ul").listview("refresh");
+                $.mobile.hidePageLoadingMsg();
+                mainloaded = true;
+
+            });
+        }
+    }
+    http.send(null);*/
+
+     
 }
 
 /************  Se realiza la configuracion de la base de datos ****************/
--- assets/js/jquery.mobile.td.mobileinit.js
+++ assets/js/jquery.mobile.td.mobileinit.js
@@ -8,33 +8,7 @@
 	the jQuery Mobile javascript file.
 */
 $(document).bind('mobileinit', function(event){
-	// apply overrides here
 	$.mobile.loadingMessage = "Cargando ...";
-        //$.mobile.loadingMessageTheme = "a"
-        //$.mobile.loadingMessageTextVisible = false;
-        $.mobile.pageLoadErrorMessage = "Error al cargar la página";
-        //$.mobile.pageLoadErrorMessageTheme = "e"
-	
-	// Navigation
-	//$.mobile.page.prototype.options.backBtnText = "Go back";
-	//$.mobile.page.prototype.options.addBackBtn      = true;
-	//$.mobile.page.prototype.options.backBtnTheme    = "d";
-	
-	// Page
-	//$.mobile.page.prototype.options.headerTheme = "a";  // Page header only
-	//$.mobile.page.prototype.options.contentTheme    = "c";
-	//$.mobile.page.prototype.options.footerTheme = "a";
-	
-	// Listviews
-	//$.mobile.listview.prototype.options.headerTheme = "a";  // Header for nested lists
-	//$.mobile.listview.prototype.options.theme           = "c";  // List items / content
-	//$.mobile.listview.prototype.options.dividerTheme    = "d";  // List divider
-	//$.mobile.listview.prototype.options.splitTheme   = "c";
-	//$.mobile.listview.prototype.options.countTheme   = "c";
-	//$.mobile.listview.prototype.options.filterTheme = "c";
-	//$.mobile.listview.prototype.options.filterPlaceholder = "Filter data...";
-	
-	//$.mobile.dialog.prototype.options.theme
-	//$.mobile.selectmenu.prototype.options.menuPageTheme
-	//$.mobile.selectmenu.prototype.options.overlayTheme
+        $.mobile.allowCrossDomainPages = true;        
+        $.mobile.pageLoadErrorMessage = "Error al cargar la página";        
 });
