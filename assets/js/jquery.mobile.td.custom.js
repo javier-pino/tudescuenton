1f4a9c2063fd94f1aa8806798f185dd4bdd86166
Javier Pino <savagej.p@gmail.com>
2012-09-10 16:49:53 -0430

    Pruebas de registros

--- assets/js/jquery.mobile.td.custom.js
+++ assets/js/jquery.mobile.td.custom.js
@@ -1,23 +1,7 @@
-//This is used to handle multiple submits events
-var lock = false;
-var server = 'http://192.168.1.102';
-var timeout = 3000; 
-
-//Mensajes a mostrar
-
-var messages = [];
-messages["TIME_OUT"] = 'Se excedió; el tiempo de espera de conexión a TuDescuentón';
-messages["JSON_NULL"] = "El servidor TuDescuentón no responde";
-messages["STATUS_NO_200"] = "El servidor TuDescuentón no responde correctamente";
-messages["NO_CITIES"] = "No es posible obtener las ciudades disponibles";
-messages["INVALID_DATE"] = 'El  campo "F. Nacimiento" no es una fecha válida: 09/06/1985'
-messages["INVALID_ID"] = 'El campo "Cédula" acepta sólo números';
-messages["INVALID_MOBILE"] = 'El campo "Tlf. Celular" acepta sólo números';
-messages["INVALID_EMAIL"] = 'El campo "Correo Electrónico" no es válido';
-messages["EMAIL_NOT_MATCH"] = 'Los campos "Correo Electrónico" y "Confirmar Correo" no coinciden';
-messages["PASSWORD_NOT_MATCH"] = 'Los campos "Contraseña" y "Confirmar Contraseña" no coinciden';
-messages["TERMS_UNCHECK"] = 'Aceptar los Términos y Condiciones es obligatorio';
-messages["PASSWORD_MIN_SIZE"] = 'El campo "Contraseña" debe tener más de 4 caracteres';
+/**
+ * En este archivo se declaran aquellas funcionalidades que se refieren al
+ * comportamiento de la aplicacion 
+ **/
 
 //Se asocian los eventos necesarios para TODAS LAS PAGINAS
 function prepare_initial_binds() {
@@ -49,7 +33,14 @@ $('#iniciar').bind('pageinit', function () {
 //Los eventos necesarios para la página...
 $('#registrar').bind('pageinit', function () {       
     prepare_initial_binds();    
-    buscar_ciudades();
+    
+    var request = new XMLHttpRequest();    
+    request.open("GET", server + '/td/restful/account/ciudades_municipios', false);
+    request_time_out(request);
+    request.onreadystatechange = function() {                
+        process_ajax_request(request, process_buscar_ciudades);        
+    };            
+    request.send();    
         
     //Cambia Municipio en caso de que se modifique ciudades
     $(document).delegate('select#city_id', 'change', function() {        
@@ -73,125 +64,8 @@ $('#registrar').bind('pageinit', function () {
     });   
 });
 
-  
-/************  Se realiza la configuracion de la base de datos ****************/
 
-//Declarar las tablas necesarias
-var TBL_User = null;
-
-/** Prepara la persistencia y la base de datos */
-function prepare_database (database, description, time) {
-    
-    //Se configura la persistencia
-    if (window.openDatabase) { //Se carga la base de datos        
-        persistence.store.websql.config(persistence, database, description, time);        
-    } else { //Usa la memoria
-        persistence.store.memory.config(persistence);
-    }      
-    persistence.debug = false;
-    
-    //Definir las tablas
-    TBL_User = persistence.define('TBL_User', {
-        user_id: "BIGINT",
-        email: "TEXT",
-        realname: "TEXT", 
-        gender: "TEXT",        
-        cedula: "TEXT"        
-    });               
-    persistence.schemaSync(); //Configura la base de datos inicial
-}
-
-/** Elimina las tablas de tu descuenton */
-function remove_database () {
-    persistence.reset();    
-}
-
-
-/*************  Métodos de envios de mensajes ******************/
-
-/** Permite declarar mensajes informativos */
-function setErrorMessage(text) {        
-    $('div#error_message h3').append('<p>' + text + '</p>');
-    $('div#error_message').show();
-    $('a[data-role=button]','div#error_message').focus();
-}
-
-/** Permite declarar mensajes informativos */
-function setInfoMessage(text) {
-    $('div#info_message h3').append('<p>' + text + '</p>');
-    $('div#info_message').show();
-    $('a[data-role=button]','div#info_message').focus();
-}
-
-function clearMessages() {
-    $('div#info_message h3').html('');
-    $('div#error_message h3').html('');
-    $('div#info_message').hide();
-    $('div#error_message').hide();
-}
-
-
-/***************** Funciones de validación del input *************/
-/** Indica si lel texto es una direccion de correo válida */
-function validEmail(dtValue) {    
-    var dtRegex = new RegExp(/^.+@.+\..+$/);
-    return dtRegex.test(dtValue);
-}
-
-/** Indica si el texto es un número válido */
-function validNumber(dtValue) {
-    if (!dtValue) 
-        return false;
-    else
-        return (!isNaN(dtValue));
-}
-
-/** Indica si el texto es una fecha válida */
-function validDate(dtValue) {
-    //Declare Regex 
-    var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
-    var dtArray = dtValue.match(rxDatePattern); 
-
-    if (dtArray == null)
-        return false;
-
-    //Checks for mm/dd/yyyy format.
-    dtDay= dtArray[1];
-    dtMonth = dtArray[3];	  
-    dtYear = dtArray[5];
-
-    if (dtMonth < 1 || dtMonth > 12)
-        return false;
-    else if (dtDay < 1 || dtDay> 31)
-        return false;
-    else if ((dtMonth==4 || dtMonth==6 || dtMonth==9 || dtMonth==11) && dtDay ==31)
-        return false;
-    else if (dtMonth == 2)
-    {
-        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
-        if (dtDay> 29 || (dtDay ==29 && !isleap))
-            return false;
-    }
-    return true;
-}
-
-/** Esta funcion evalua todos aquellos inputs, que estén visibles en el momento
- *actual, cuya clase sea required y retorna falso, si algun valor no esta lleno*/
-function validInput($form) {   
-    var validate = true;
-    $form.find("[data-required='true']:visible").each(
-        function () {        
-            if (!$(this).val() || $(this).val() == '' ) {                
-                setErrorMessage($(this).attr('data-message'));
-                validate = false;             
-                return false;
-            }        
-    });
-    return validate;
-}
-
-/*************** Métodos de ajax *************/
-/** Funcion que valida el input */
+/** Inicia el submit */
 function iniciar_submit() {
 
     $.mobile.showPageLoadingMsg ();       
@@ -208,114 +82,12 @@ function iniciar_submit() {
     var request = new XMLHttpRequest();    
     request.open("GET", server + '/td/restful/account/login' 
         + '?email=' + email + '&password=' + password, true);
-    
-    //Se asocia el timeout
-    var xmlHttpTimeout = setTimeout(function () {                   
-        if (request.readyState < 4 ) {             
-            request.abort();                    
-            setErrorMessage(messages["TIME_OUT"]);                                    
-        }   
-        $.mobile.hidePageLoadingMsg ();                        
-    }, timeout);      
-
-    //Se asocia el 
-    request.onreadystatechange = function() {
-        
-        if (request.readyState != 4)  { return; }                
-        if (request.status == 200) {
-            
-            var json = null;
-            if (request.responseText != null && request.responseText != '') {                
-                json = JSON.parse(request.responseText);                
-            }
-            if (json != null) {
-                if (json.status) {
-                        TBL_User.all().one(null, function (one) {                    
-
-                        if (one != null) {                        
-                            persistence.remove(one);  
-                        }                                     
-                        var user = new TBL_User();                        
-                        user.user_id = json.user.id;
-                        user.email = json.user.email;
-                        user.gender = json.user.gender;
-                        user.cedula = json.user.cedula;
-                        user.realname = json.user.realname;
-                        persistence.add(user);
-
-                        persistence.flush(null, function () {
-                            $.mobile.hidePageLoadingMsg ();                                                                                          
-                            setInfoMessage(
-                                'Bienvenid@, ' + json.user.realname +
-                                ', ingresaste exitosamente usando tu correo: ' 
-                                + json.user.email);                
-                        });
-                    });                                 
-                } else {
-                    setErrorMessage(json.message);
-                    $.mobile.hidePageLoadingMsg ();          
-                }
-            } else {
-                setErrorMessage(messages["JSON_NULL"]);            
-                $.mobile.hidePageLoadingMsg (); 
-            }
-        } else {
-            setErrorMessage(messages["STATUS_NO_200"]);            
-            $.mobile.hidePageLoadingMsg (); 
-        }
-    };   
-    
+    request_time_out(request);
+    request.onreadystatechange = function() {                
+        process_ajax_request(request, process_iniciar_sesion);        
+    };            
     request.send();    
 }
-
-
-/** Busca las ciudades y municipios necesarios */
-function buscar_ciudades () {
-
-    var request = new XMLHttpRequest();    
-    request.open("GET", server + '/td/restful/account/ciudades_municipios', false);
-    
-    //Se asocia el timeout
-    var xmlHttpTimeout = setTimeout(function () {                   
-        if (request.readyState < 4 ) {             
-            request.abort();                    
-            setErrorMessage(messages["NO_CITIES"]);                                    
-        }           
-    }, timeout);      
-
-    //Se asocia el 
-    request.onreadystatechange = function() {        
-        if (request.readyState != 4)  { return; }                
-        if (request.status == 200) {            
-            var json = null;
-            if (request.responseText != null && request.responseText != '')
-                json = JSON.parse(request.responseText);
-            if (json != null) {
-                $select = $('select#city_id');
-                $select.html('<option value="">Selecciona tu ciudad</option>');                
-                $.each(json['ciudades'], function (key, val) { 
-                    $select.append(
-                        "<option value='" + key + "'>" + val + "</option"                    
-                    );                    
-                });   
-                $select.selectmenu('refresh');
-                
-                $select = $('select#city_select');                
-                $select.html('<option value="">Selecciona tu municipio</option>');
-                $.each(json['municipios'], function (key, val) {                    
-                    $select.append(
-                        "<option value='" + key + "'>" + val + "</option"                    
-                    );                    
-                });
-                $select.selectmenu('refresh');
-                return;
-            }
-        }
-        setErrorMessage(messages["NO_CITIES"]);
-    };       
-    request.send();    
-}
-
 
 /** Función que registra al usuario */
 function registrar_usuario() {
@@ -391,75 +163,79 @@ function registrar_usuario() {
         $.mobile.hidePageLoadingMsg ();
         return;
     }
-    
-    //Se crea la variable post
+       
+    //Se crea la variable post y se realiza la petición
     post = encodeURI($form.serialize());
-   
     var request = new XMLHttpRequest();         
     request.open("POST", server + '/td/restful/account/register', true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");    
-        
-    //Se asocia el timeout
-    var xmlHttpTimeout = setTimeout(function () {                   
-        if (request.readyState < 4 ) {             
-            request.abort();                    
-            setErrorMessage(messages["TIME_OUT"]);                                    
-        }   
-        $.mobile.hidePageLoadingMsg ();                        
-    }, timeout);      
-
-    //Se asocia el 
-    request.onreadystatechange = function() {
-
-        if (request.readyState != 4)  { return; }                
-        if (request.status == 200) {
-
-            alert(request.readyState);
-            alert(request.status);
-            alert(request.responseText);
-
-            var json = null;
-            if (request.responseText != null && request.responseText != '') {                
-                json = JSON.parse(request.responseText);                
-            }
-            if (json != null) {
-                if (json.status) {                    
-                    alert('status fine');                        
-                    /*                        
-                        TBL_User.all().one(null, function (one) {                    
-
-                        if (one != null) {                        
-                            persistence.remove(one);  
-                        }                                     
-                        var user = new TBL_User();                        
-                        user.user_id = json.user.id;
-                        user.email = json.user.email;
-                        user.gender = json.user.gender;
-                        user.cedula = json.user.cedula;
-                        user.realname = json.user.realname;
-                        persistence.add(user);
-
-                        persistence.flush(null, function () {
-                            $.mobile.hidePageLoadingMsg ();                                                                                          
-                            setInfoMessage(
-                                'Bienvenid@, ' + json.user.realname +
-                                ', ingresaste exitosamente usando tu correo: ' 
-                                + json.user.email);                
-                        });
-                    });    
-                     */                             
-                } else {
-                    setErrorMessage(json.message);
-                    $.mobile.hidePageLoadingMsg ();          
-                }
-            } else {
-                setErrorMessage(messages["JSON_NULL"]);            
-                $.mobile.hidePageLoadingMsg (); 
-            }
-        } else {
-            setErrorMessage(messages["STATUS_NO_200"]);            
-            $.mobile.hidePageLoadingMsg (); 
-        }
-    };       
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");                
+    request_time_out(request);
+    request.onreadystatechange = function() {                
+        process_ajax_request(request, process_registrar_usuario);        
+    };        
     request.send(post);            
+}
+
+/** Esta función procesa el json de iniciar_sesion */
+function process_iniciar_sesion (json) {
+    if (json.status) {
+        TBL_User.all().one(null, function (one) {                    
+            if (one != null) {                        
+                persistence.remove(one);  
+            }                                     
+            var user = new TBL_User();                        
+            user.user_id = json.user.id;
+            user.email = json.user.email;
+            user.gender = json.user.gender;
+            user.cedula = json.user.cedula;
+            user.realname = json.user.realname;
+            persistence.add(user);
+
+            persistence.flush(null, function () {
+                $.mobile.hidePageLoadingMsg ();                                                                                          
+                setInfoMessage(
+                    'Bienvenid@, ' + json.user.realname +
+                    ', ingresaste exitosamente usando tu correo: ' 
+                    + json.user.email);                
+            });
+        });                                 
+    } else {
+        setErrorMessage(json.message);
+        $.mobile.hidePageLoadingMsg ();          
+    }   
+}
+
+/** Esta función procesa el json de registrar usuario */
+function process_registrar_usuario (json) {      
+    if (json.status) {                                                            
+        $.mobile.hidePageLoadingMsg ();                                                                                          
+        setInfoMessage(
+            'Bienvenid@, ' + json.user.realname +
+            ', te has registrado exitosamente y pronto se te enviará un correo de confirmación');                                                               
+    } else {
+        setErrorMessage(json.message);
+        $.mobile.hidePageLoadingMsg ();          
+    }
+}
+
+/**- Esta función procesa la informacióin recibida por buscar_ciudades */
+function process_buscar_ciudades (json) {   
+    $select = $('select#city_id');
+    $select.html('<option value="">Selecciona tu ciudad</option>');                
+    $.each(json['ciudades'], function (key, val) { 
+        $select.append(
+            "<option value='" + key + "'>" + val + "</option"                    
+        );                    
+    });   
+    $select.selectmenu('refresh');
+
+    $select = $('select#city_select');                
+    $select.html('<option value="">Selecciona tu municipio</option>');
+    $.each(json['municipios'], function (key, val) {                    
+        $select.append(
+            "<option value='" + key + "'>" + val + "</option"                    
+        );                    
+    });
+    $select.selectmenu('refresh');
+    return;    
 }
---
+++ assets/js/jquery.mobile.td.functions.js
@@ -0,0 +1,175 @@
+/**
+ * En este archivo se declaran aquellas funcionalidades que se refieren a las operaciones
+ * y los datos de la aplicación
+ * 
+ * Autor: Javier Pino
+ * Fecha 10/09/2012
+ */
+var lock = false;
+var server = 'http://107.22.208.22';
+var timeout = 3000; 
+
+/************** FUNCIONES RELACIONADAS CON EL ENVIO DE MENSAJES ****************/
+
+var messages = []; //Mensajes a mostrar
+messages["TIME_OUT"] = 'Se excedió el tiempo de espera de conexión a TuDescuentón';
+messages["JSON_NULL"] = "El servidor TuDescuentón no responde";
+messages["STATUS_NO_200"] = "El servidor TuDescuentón no responde correctamente";
+messages["NO_CITIES"] = "No es posible obtener las ciudades disponibles";
+messages["INVALID_DATE"] = 'El  campo "F. Nacimiento" no es una fecha válida: 09/06/1985'
+messages["INVALID_ID"] = 'El campo "Cédula" acepta sólo números';
+messages["INVALID_MOBILE"] = 'El campo "Tlf. Celular" acepta sólo números';
+messages["INVALID_EMAIL"] = 'El campo "Correo Electrónico" no es válido';
+messages["EMAIL_NOT_MATCH"] = 'Los campos "Correo Electrónico" y "Confirmar Correo" no coinciden';
+messages["PASSWORD_NOT_MATCH"] = 'Los campos "Contraseña" y "Confirmar Contraseña" no coinciden';
+messages["TERMS_UNCHECK"] = 'Aceptar los Términos y Condiciones es obligatorio';
+messages["PASSWORD_MIN_SIZE"] = 'El campo "Contraseña" debe tener más de 4 caracteres';
+
+/** Permite declarar mensajes informativos */
+function setErrorMessage(text) {        
+    $('div#error_message h3').append('<p>' + text + '</p>');
+    $('div#error_message').show();
+    $('a[data-role=button]','div#error_message').focus();
+}
+
+/** Permite declarar mensajes informativos */
+function setInfoMessage(text) {
+    $('div#info_message h3').append('<p>' + text + '</p>');
+    $('div#info_message').show();
+    $('a[data-role=button]','div#info_message').focus();
+}
+
+function clearMessages() {
+    $('div#info_message h3').html('');
+    $('div#error_message h3').html('');
+    $('div#info_message').hide();
+    $('div#error_message').hide();
+}
+
+/********************* FUNCIONES DE VALIDACIÓN DE INPUT *********************/
+
+/** Indica si lel texto es una direccion de correo válida */
+function validEmail(dtValue) {    
+    var dtRegex = new RegExp(/^.+@.+\..+$/);
+    return dtRegex.test(dtValue);
+}
+
+/** Indica si el texto es un número válido */
+function validNumber(dtValue) {
+    if (!dtValue) 
+        return false;
+    else
+        return (!isNaN(dtValue));
+}
+
+/** Indica si el texto es una fecha válida */
+function validDate(dtValue) {
+    //Declare Regex 
+    var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+    var dtArray = dtValue.match(rxDatePattern); 
+
+    if (dtArray == null)
+        return false;
+
+    //Checks for mm/dd/yyyy format.
+    dtDay= dtArray[1];
+    dtMonth = dtArray[3];	  
+    dtYear = dtArray[5];
+
+    if (dtMonth < 1 || dtMonth > 12)
+        return false;
+    else if (dtDay < 1 || dtDay> 31)
+        return false;
+    else if ((dtMonth==4 || dtMonth==6 || dtMonth==9 || dtMonth==11) && dtDay ==31)
+        return false;
+    else if (dtMonth == 2)
+    {
+        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+        if (dtDay> 29 || (dtDay ==29 && !isleap))
+            return false;
+    }
+    return true;
+}
+
+/** Esta funcion evalua todos aquellos inputs, que estén visibles en el momento
+ *actual, cuya clase sea required y retorna falso, si algun valor no esta lleno*/
+function validInput($form) {   
+    var validate = true;
+    $form.find("[data-required='true']:visible").each(
+        function () {        
+            if (!$(this).val() || $(this).val() == '' ) {                
+                setErrorMessage($(this).attr('data-message'));
+                validate = false;             
+                return false;
+            }        
+    });
+    return validate;
+}
+
+/********************* FUNCIONES DE PETICIONES AJAX *********************/
+
+/** Procesa las peticiones ajax, toma como parámetro la función a llamar */
+function process_ajax_request(request, callback) {
+
+    if (request.readyState != 4)  { return; }                
+    if (request.status == 200) {            
+        var json = null;
+        if (request.responseText != null && request.responseText != '') {                
+            json = JSON.parse(request.responseText);                
+        }
+        if (json != null) {
+            callback(json);
+        } else {
+            setErrorMessage(messages["JSON_NULL"]);            
+            $.mobile.hidePageLoadingMsg (); 
+        }
+    } else {
+        setErrorMessage(messages["STATUS_NO_200"]);            
+        $.mobile.hidePageLoadingMsg (); 
+    }   
+}
+
+/** Esta función asigna un timeoput a una peticion */
+function request_time_out(request, message) {
+    
+    //Se crea el timeout para esa peticion
+    var xmlHttpTimeout = setTimeout(function () {                   
+        if (request.readyState < 4 ) {             
+            request.abort();             
+            setErrorMessage(messages["TIME_OUT"]);            
+        }   
+        $.mobile.hidePageLoadingMsg ();                        
+    }, timeout);
+}
+
+/**************** FUNCIONES DE CONFIGURACION DE LA BD *********************/
+
+//Declarar las tablas necesarias
+var TBL_User = null;
+
+/** Prepara la persistencia y la base de datos */
+function prepare_database (database, description, time) {
+    
+    //Se configura la persistencia
+    if (window.openDatabase) { //Se carga la base de datos        
+        persistence.store.websql.config(persistence, database, description, time);        
+    } else { //Usa la memoria
+        persistence.store.memory.config(persistence);
+    }      
+    persistence.debug = false;
+    
+    //Definir las tablas
+    TBL_User = persistence.define('TBL_User', {
+        user_id: "BIGINT",
+        email: "TEXT",
+        realname: "TEXT", 
+        gender: "TEXT",        
+        cedula: "TEXT"        
+    });               
+    persistence.schemaSync(); //Configura la base de datos inicial
+}
+
+/** Elimina las tablas de tu descuenton */
+function remove_database () {
+    persistence.reset();    
+}
